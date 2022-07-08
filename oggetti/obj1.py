6b4a5de8a026b43c680e022759718812cbc4002f
linux79 <idkfaitu534@gmail.com>
2022-07-08 14:02:24 +0200

    boot int

---
+++ oggetti/obj1.py
@@ -0,0 +1,10 @@
+class User:
+    def __init__(self,permessi,azioni,var,etc):
+        self.permessi = permessi
+        self.azioni = azioni
+        self.var = var
+        self.etc = etc
+
+def rescue(self):
+    if FileNotFoundError() is True:
+        input("grub rescue ")
