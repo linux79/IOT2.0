43f9e4b519da6f7c3ce3faec76848d5e26702aa7
linux79 <idkfaitu534@gmail.com>
2022-07-10 19:49:15 +0200

    repo

---
+++ sys/gnu_packages.py
@@ -0,0 +1,24 @@
+utente = None
+repo1 = {'flatpak','snap-store','official-packages'}
+repo2 = {'chocolatey','aur','appimage','extra'}
+class package:
+    
+    def __init__(self,compile_method,license,extension):
+        self.compile_method = "compiled"
+        self.license = "license"
+        self.extension = "extension"
+    
+    def install_packages():
+            if input() == "gnu-murd -install {}":
+                try:    
+                    input("paste build() ")
+                    while input == "":
+                         input("paste build() ")
+                except Exception:
+                        while "usr" not in "build.sh":
+                            print("exit code -1")
+                            pass
+                print("installing packages")
+                while utente not in repo1 or repo2:
+                    print("inpossible to find packages")
+                    pass
